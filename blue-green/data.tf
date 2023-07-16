9436702f3b18ef97d5cb9db92424b5464df6616a
HopeEngr <swetha.rayala@gmail.com>
2023-07-16 15:21:15 -0400

    Blue Deplyment

--- blue-green/data.tf
+++ blue-green/data.tf
@@ -17,5 +17,15 @@ data "aws_ami" "windows" {
   }
 }
 
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 
 
--- blue-green/outputs.tf
+++ blue-green/outputs.tf
@@ -7,3 +7,44 @@ output "availability_zones" {
 output "windows_ami" {
   value = data.aws_ami.windows.id
 }
+
+#output linux ami
+output "linux_ami" {
+  value = data.aws_ami.amazon_linux.id
+}
+
+#output vpc id
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+#output subnet ids
+output "public_subnet_ids" {
+  value = module.vpc.public_subnets
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnets
+}
+
+#output security group ids
+output "app_security_group_id" {
+  value = module.app_security_group.security_group_id
+}
+
+output "lb_security_group_id" {
+  value = module.lb_security_group.security_group_id
+}
+
+#output security group names
+output "app_security_group_name" {
+  value = module.app_security_group.security_group_name
+}
+
+output "lb_security_group_name" {
+  value = module.lb_security_group.security_group_name
+}
+
+output "lb_dns_name" {
+  value = aws_lb.app.dns_name
+}
